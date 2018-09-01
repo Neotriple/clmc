d66c558cf96d9bfedbc52355cede29ba6bf8f49b
Neotriple <Neotriple@gmail.com>
2018-08-31 17:10:27 -0700

    need to fix explicit pointer stuff

--- mpc_demo.py
+++ mpc_demo.py
@@ -10,6 +10,7 @@ from os.path import join
 from pinocchio.utils import *
 from pinocchio.robot_wrapper import RobotWrapper
 import time
+import math
 
 import pyipopt
 
@@ -22,7 +23,8 @@ if __name__ == "__main__":
 
     robot.initDisplay(loadModel=True)
 
-    qInit = rand(robot.nq)
+    qInit = np.array([0.59346963, 0.25128594, 0.62805152, 0.17837691, 0.86218068, 0.3577253])
+    qInit = np.asmatrix(qInit).T
 
     se3.forwardKinematics(robot.model, robot.data, qInit)
 
@@ -41,11 +43,12 @@ if __name__ == "__main__":
 
     #PyIpopt stuff
     timeSteps = 3
-    dt = 0.001
+    dt = .01
     nvar = robot.nv*timeSteps
     ncon = 1
 
-    q_ref1 = np.array([-1.49411011e-02,-1.32003896e+00,2.09324591e+00,-7.73206945e-01,-1.49411010e-02,-2.32376275e-09])
+    #q_ref1 = np.array([-1.49411011e-02,-1.32003896e+00,2.09324591e+00,-7.73206945e-01,-1.49411010e-02,-2.32376275e-09])
+    q_ref1 = np.array([0.59346963,0.25128594,0.62805152,0.17837691,0.86218068,0.3577253])
 
     q_ref1 = np.asmatrix(q_ref1).T
 
@@ -56,8 +59,8 @@ if __name__ == "__main__":
         qRef[temp, 0] = q_ref1
         temp += 1
 
-    x_L = np.ones((nvar), dtype=np.float_) * 0.001
-    x_U = np.ones((nvar), dtype=np.float_) * 20
+    x_L = np.ones((nvar), dtype=np.float_) * 0
+    x_U = np.ones((nvar), dtype=np.float_) * 5
     g_L = np.array([0.01])
     g_U = np.array([2.0*pow(10.0, 19)])
     #When starting off from 0 initial position & 0 initial velocity
@@ -86,7 +89,25 @@ if __name__ == "__main__":
         while (t < timeSteps):
             tau_k = tau[t*robot.nv:(t*robot.nv)+robot.nv]
             tau_k = np.asmatrix(tau_k).T
+            print
+            print("vq before rnea")
+            print(vq)
             b_k = se3.rnea(tempRobot.model, tempRobot.data, q, vq, aq)
+            if (math.isnan(b_k[0])):
+                print
+                print("t")
+                print(t)
+                print
+                print("tau")
+                print(tau)
+                print
+                print("q")
+                print(q)
+                print
+                print("vq")
+                print(vq)
+                time.sleep(100)
+                exit()
             M_k = se3.crba(tempRobot.model, tempRobot.data, q)
             aq = np.linalg.inv(M_k)*(tau_k - b_k)
             vq += dt*aq
@@ -97,7 +118,6 @@ if __name__ == "__main__":
         #qNorm = np.linalg.norm(qRef - qNorm)
         qNorm = agnp.linalg.norm(qNorm-qRef)
         #return vectorNorm + qNorm
-        print(qNorm)
         return vectorNorm + qNorm
 
 
@@ -108,12 +128,21 @@ if __name__ == "__main__":
     def eval_grad_f_finiteDiff(tau):
 
         grad_f = np.empty([timeSteps*robot.nv])
-        diffStep = dt/100
-
+        diffStep = dt/10
         i = 0
         while(i < len(tau)):
+            print("tau actual")
+            print(tau)
+            print
             tauTemp = tau
             tauTemp[i] = tau[i] + diffStep
+            print("tau actual again")
+            print(tau)
+            print
+            print("tau_temp")
+            print(tauTemp)
+            print
+            time.sleep(10)
             upperLim = eval_f(tauTemp)
             tauTemp[i] = tau[i] - diffStep
             bottomLim = eval_f(tauTemp)
@@ -166,16 +195,15 @@ if __name__ == "__main__":
     print "f(x*) =", obj
 
 
-    q = q0
-    vq = zero(robot.nv)
-    t = 0
-    while (t < timeSteps):
-        tau_k = traj[t*robot.nv:(t*robot.nv)+robot.nv]
-        tau_k = np.asmatrix(tau_k).T
-        aq = se3.aba(robot.model, robot.data, q, vq, tau_k)
-        vq += aq * dt
-        q = se3.integrate(robot.model, q, vq*dt)
-        robot.display(q)
-        t += 1
-        time.sleep(2.5)
+    # q = q0
+    # vq = zero(robot.nv)
+    # t = 0
+    # while (t < timeSteps):
+    #     tau_k = traj[t*robot.nv:(t*robot.nv)+robot.nv]
+    #     tau_k = np.asmatrix(tau_k).T
+    #     aq = se3.aba(robot.model, robot.data, q, vq, tau_k)
+    #     vq += aq * dt
+    #     q = se3.integrate(robot.model, q, vq*dt)
+    #     robot.display(q)
+    #     t += 1
 
